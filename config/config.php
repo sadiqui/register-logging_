153489247ba10e04f42963c4e51af9a97bede7dd
Abdelilah SADIQUI <143453064+sadiqui@users.noreply.github.com>
2023-12-18 01:26:15 +0100

    - config directory

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "auth");
-define('URLROOT', 'http://localhost/OOP_PHP_Auth');
-define('USERNAME', 'admin@gmail.com');
-define('PASSWORD', '12345');
-?>
